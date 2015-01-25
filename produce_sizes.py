274c4d5c788c806129fc8a97d8fd95ef92f860b6
gsiatras <gsiatras@hotmail.com>
2015-01-26 01:02:42 +0200

    Updated scripts

--- produce_sizes.py
+++ produce_sizes.py
@@ -1,3 +1,3 @@
 for p in range(8, 15):
-    for q in range(8, p + 1):
+    for q in range(4, p + 1):
         print str(2**p) + " " + str(2**q)
--- runtests.sh
+++ runtests.sh
@@ -11,19 +11,43 @@ echo $(make all)
 
 echo "A long series of tests will run now.. Please wait!"
 
+
 while read m n; do
 
     echo -e "\n------------- M="$m" N="$n", 3 iterations -------------"
 
-    printf "${green}PART A: ${normal}\t| ${blue}PART B: ${normal}\t| ${red}PART C: ${normal} \n"
-    
+    if [[ "$RUN_EXPERIMENTS_VB" = "1" ]]
+    then
+    	printf "${green}PART A: ${normal}\t| ${blue}PART B: ${normal}\t| ${red}PART C: ${normal} \n"
+    fi
+
+    sum1=0
+    sum2=0
+    sum3=0
+
     for iter in {1..3};
     do
-        b1=$((./mainA "$m" "$n" | grep -iF "Elapsed" | awk '{print $6 " " $7}') 2>&1)
-        b2=$((./mainB "$m" "$n" | grep -iF "Elapsed" | awk '{print $6 " " $7}') 2>&1)
-        b3=$((./mainC "$m" "$n" | grep -iF "Elapsed" | awk '{print $6 " " $7}') 2>&1)
-        echo "$b1, $b2, $b3"
+        b1=$((./mainA "$m" "$n" | grep -iF "Elapsed" | awk '{print $6}') 2>&1)
+        b2=$((./mainB "$m" "$n" | grep -iF "Elapsed" | awk '{print $6}') 2>&1)
+        b3=$((./mainC "$m" "$n" | grep -iF "Elapsed" | awk '{print $6}') 2>&1)
+
+	if [[ "$RUN_EXPERIMENTS_VB" = "1" ]]
+	then
+        	echo "$b1 msecs, $b2 msecs, $b3 msecs"
+	fi
+
+	sum1=$(echo $b1 + $sum1 | bc)
+	sum2=$(echo $b2 + $sum2 | bc)
+	sum3=$(echo $b3 + $sum3 | bc)
+
     done
+
+    avg1=$(echo "scale=8; $sum1/3" | bc -l)
+    avg2=$(echo "scale=8; $sum2/3" | bc -l)
+    avg3=$(echo "scale=8; $sum3/3" | bc -l)
+
+    printf "${green}Average ${normal}\t| ${blue}Average ${normal}\t| ${red}Average ${normal} \n"
+    echo "$avg1 msecs, $avg2 msecs, $avg3 msecs"
 
 done < expsizes
 
